e108299d48c5f0e31dcc63f2ac74352cac9a772a
Sindon20 <ginessindonjhamaicamae@gmail.com>
2025-02-26 12:14:14 +0800

    Add files via upload

---
+++ activity1.js
@@ -0,0 +1,10 @@
+// variables activity.js
+let Name = "Jhamaica Mae";
+let whatdoyouwanttobecomeInyourlife = "Soft Engineering";
+let gender = "Female";
+let IgHandle = "@Its micaaaaaaaa";
+
+console.log ("Name:", Name);
+console.log ("Future Profession:", whatdoyouwanttobecomeInyourlife);
+console.log ("gender:", gender);
+console.log ("Ig Handle:", IgHandle);
---
+++ activity5.js
@@ -0,0 +1,16 @@
+//strings.js
+let favArtistFirstNamae = "Andrea";
+let favArtistLastNamae = "Brillantes";
+let fullName = favArtistFirstNamae + " " + favArtistLastNamae;
+let uppercase = "fullName, toUpperCase()";
+let message = 'My favorite artist is $ {uppercase}, She is the number one beautiful in the world.';
+
+console.log ("First Name;", favArtistFirstNamae);
+console.log ("Last Name;", favArtistLastNamae);
+console.log ("Full Name;", fullName);
+console.log ("Uppercase;", uppercase);
+console.log ("message;", message);
+
+
+
+
